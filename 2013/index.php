30ccefe139b39ec9a0747661cc2d175f7bfb31b9
Hein Haraldson Berg <hein@keyteq.no>
2013-03-12 14:01:51 +0100

    Initial commit: Up and running with Compass Sass

---
+++ 2013/index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <head>
+            <meta charset="utf-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=1" />
+            <meta name="apple-mobile-web-app-capable" content="no" />
+            <title>Hein Haraldson Berg</title>
+            <link rel="stylesheet" type="text/css" href="css/screen.css" />
+            <link rel="made" href="http://haraldson.ch/" title="Hein Haraldson Berg" />
+            <link rel="author" href="http://haraldson.ch/" />
+            <link rel="icon" href="gfx/favicon.png" type="image/png" />
+            <link rel="shortcut icon" href="gfx/favicon.png" type="image/png" />
+            <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        </head>
+    </head>
+    <body>
+        <div id="container">
+            <h1>Some title</h1>
+            <h2>Some caption</h2>
+            <p>Some lorem ipsum dolor sit amet</p>
+        </div>
+        <script src="js/fn.js"></script>
+    </body>
+</html>
